2cea01f38093df14c485a8793eb36833b445d0a6
YujeongLEE <dldbwjd24@gmail.com>
2024-08-28 00:10:12 +0900

    reverse string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,27 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    let reversed = ""
+
+    for(let char of str){
+        reversed = char + reversed
+    }
+    return reversed
+}
+
+
+function reverse2(str) {
+    return str.split('') //make str to Array
+        .reverse()//reverse it
+        .join('')//make array to str
+}
+
+function reverse3(str) {
+    return str.split('')
+        .reduce((reversed, char) =>  // (last return of arrow function, str.split('') array 
+            char + reversed
+            ,'') //initial value of arrow function
+}
 
 module.exports = reverse;

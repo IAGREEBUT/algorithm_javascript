1400a1e3ae3278420da1773fd0461e025e4b3ed2
YujeongLEE <dldbwjd24@gmail.com>
2024-08-31 21:53:08 +0900

    max char

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,32 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const char = {}
+
+    for( let c of str){
+        char[c] = char[c] + 1 || 1 // if char[c] is null, null+1 is null which refers falsy, then char[c] became 1
+
+        // if(char[c]){
+        //     char[c] ++
+        // }else{
+        //     char[c] = 1
+        // }
+    }
+
+    let maxValue = 0
+    let maxKey = ''
+
+    for( let key in char){
+        if(char[key] > maxValue){
+            maxValue = char[key]
+            maxKey = key
+        }
+    }
+
+    return maxKey
+
+}
+
 
 module.exports = maxChar;

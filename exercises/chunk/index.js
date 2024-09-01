1836349990a791b50a14a0c86f601375e44faed4
YujeongLEE <dldbwjd24@gmail.com>
2024-09-01 23:54:24 +0900

    chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,61 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+    let arrays = []
+    let subArray = []
+    for( let num of array){
+        subArray.push(num)
+
+        if(subArray.length === size){
+            arrays.push(subArray)
+            subArray = []
+        }
+    }
+
+    if(subArray.length !== 0){
+        arrays.push(subArray)
+    }
+
+    return arrays
+
+}
+
+
+
+function chunk2(array, size)
+{
+
+    const chunked = []// array of chunk
+
+    for(let element of array){
+        const last = chunked[chunked.length-1] // last element of chunked
+
+        if(!last || last.length === size){ //1. not exist -> push new array(with element) 2. fulled -> push new array(with element)
+            chunked.push([element])
+        }else{//not fulled yet -> push new element to the last
+            last.push(element)
+        }
+
+    }
+
+}
+
+
+function chunk3(array, size)
+{
+
+    const chunked = []// array of chunk
+
+    let idx = 0
+    while(idx < array.length){
+        chunked.push(array.slice(idx, idx+size))
+        idx += size
+    }
+
+    return chunked
+
+}
+
 
 module.exports = chunk;

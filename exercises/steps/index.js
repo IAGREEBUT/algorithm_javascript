48ee3d53cd28ee768b72bea24835189b5b2ee154
YujeongLEE <dldbwjd24@gmail.com>
2024-09-05 00:30:59 +0900

    [중요] steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,47 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps2(n) {
+    for(let i=1; i<=n; i++){
+        let str = '';
+        for(let j = 1 ; j<=i ; j++){
+            str += "#"
+        }
+        for(let k = 1; k<=n-i ; k++){
+            str += " "
+        }
+        console.log(str)
+    }
+}
+
+
+function steps(n) {
+    for(let i=1; i<=n; i++){
+        let str = '';
+        for(let j = 1 ; j<=n ; j++){
+            if(j<=i) str += "#"
+            else str += " "
+        }
+        console.log(str)
+    }
+}
+
+
+function stepRecursion(n, row = 0, stair = ''){
+
+    if(n==row) return; //completed all of work
+
+    if(stair.length === n){
+        console.log(stair)
+        return stepRecursion(n,row+1) //go for next row
+    }
+
+    if(stair.length <= row){
+        stair += "#"
+    }else {
+        stair += " "
+    }
+    stepRecursion(n,row, stair) //go for same row (attach the character)
+}
 
 module.exports = steps;

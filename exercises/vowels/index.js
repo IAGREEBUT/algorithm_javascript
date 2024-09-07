d9eeb2b8a54e79f888b9c2328dbbfe77ee210e79
YujeongLEE <dldbwjd24@gmail.com>
2024-09-07 23:45:23 +0900

    vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,31 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+    let count = 0
+    for(let char of str.toLowerCase()){
+        if(char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u') count++
+    }
+    return count
+}
+
+function vowels2(str) {
+    let count = 0
+    const checker = ['a','e','i','o','u'] //also works with "aeiou" but array is
+    for(let char of str.toLowerCase()){
+        if(checker.includes(char)) count++
+    }
+    return count
+}
+
+function vowels3(str) {
+
+    //i: case insensitive
+    //g: dont stop til the end
+    const matches = str.match(/[aeioe]/gi)  //return
+
+    return matches ? matches.length : 0
+}
+
 
 module.exports = vowels;

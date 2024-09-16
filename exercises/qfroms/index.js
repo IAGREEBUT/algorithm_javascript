8216e48906f4e47a4d9091a5cec6eeba0078c25c
YujeongLEE <dldbwjd24@gmail.com>
2024-09-16 19:08:11 +0900

    queue from stack

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -14,6 +14,45 @@
 
 const Stack = require('./stack');
 
-class Queue {}
+class Queue {
+    constructor() {
+        this.first = new Stack()
+        this.second = new Stack()
+    }
+
+     add(record){
+        this.first.push(record)
+     }
+
+     remove(){
+        while(this.first.peek()){
+            this.second.push(this.first.pop())
+        }
+
+        const last = this.second.pop()
+
+         while(this.second.peek()){
+             this.first.push(this.second.pop())
+         }
+
+         return last
+     }
+
+     peek(){
+         while(this.first.peek()){
+             this.second.push(this.first.pop())
+         }
+
+         const last = this.second.peek()
+
+         while(this.second.peek()){
+             this.first.push(this.second.pop())
+         }
+         
+         return last
+     }
+
+
+}
 
 module.exports = Queue;

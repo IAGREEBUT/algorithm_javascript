da3ad970234a51bd3eaaa254131c438644e0ae3a
YujeongLEE <dldbwjd24@gmail.com>
2024-09-17 13:20:03 +0900

    linked list

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,8 +2,43 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+    data
+    next
 
-class LinkedList {}
+    constructor(data, next = null) { //default = null (not undefined)-
+        this.data = data
+        this.next = next
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null
+    }
+
+    insertFirst(data){
+        this.head = new Node(data, this.head)
+        // if(!head){
+        //     this.head = new Node(data)
+        // }else{
+        //     const firstNode = new Node(data)
+        //     firstNode.next = this.head
+        //     this.head = firstNode
+        // }
+    }
+
+    size(){
+        let count = 0
+        let node = this.head
+        while(node){
+            count++
+            node = node.next
+        }
+        return count
+    }
+
+
+}
 
 module.exports = { Node, LinkedList };
